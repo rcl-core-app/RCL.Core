e7d027fef45f8685dad52b2ae7b49d8213f015d2
Anil Ripla <support@rclapp.com>
2023-04-06 02:55:13 -0400

    Add project files.

---
+++ src/RCL.Core.Azure.BlobStorage/Abstractions/IAzureBlobStorageService.cs
@@ -0,0 +1,36 @@
+﻿using Azure.Storage.Blobs;
+
+namespace RCL.Core.Azure.BlobStorage
+{
+    public interface IAzureBlobStorageService
+    {
+        Task<BlobContainerClient> CreateOrGetContainerAsync(string ContainerName, 
+            ContainerType ContainerType);
+
+        Task<int> DeleteContainerAsync(string ContainerName);
+
+        Task<BlobClient> UploadBlobAsync(string ContainerName,
+              ContainerType ContainerType, string BlobName,
+              Stream Stream, string ContentType);
+
+        Task<BlobClient> UploadBlobAsync(string ContainerName,
+            ContainerType ContainerType, string BlobName,
+            byte[] byteArray, string ContentType);
+
+        string GetBlobSasUri(string ContainerName, string BlobName);
+
+        string GetBlobUri(string ContainerName, string BlobName);
+
+        Task<MemoryStream> DownloadBlobToStreamAsync(string ContainerName, 
+            string BlobName);
+
+        Task<byte[]> DownloadBlobToByteArrayAsync(string ContainerName, string BlobName);
+
+        Task<string> ReadTextFromBlobAsync(string ContainerName, string BlobName);
+
+        Task WriteTextToBlobAsync(string ContainerName, ContainerType ContainerType, 
+            string BlobName, string Content);
+
+        Task<int> DeleteBlobAsync(string ContainerName, string BlobName);
+    }
+}
